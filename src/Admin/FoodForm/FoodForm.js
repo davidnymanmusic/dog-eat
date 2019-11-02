3f67801c201d920c07fe2c2020ad4041a64b8b1e
David Nyman <davenyman@gmail.com>
2019-11-01 21:15:57 -0400

    Fix sort issues

--- src/Admin/FoodForm/FoodForm.js
+++ src/Admin/FoodForm/FoodForm.js
@@ -1,34 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { useToggle } from '../../hooks/useToggle';
-// import api from '../../api';
-import axios from 'axios';
-import TagSelect from '../../components/TagSelect/TagSelect';
-import FoodTable from './FoodTable';
-import { APP_URL } from '../../constants';
-import Modal from '../../components/Modal';
+import { useToggle } from "../../hooks/useToggle";
+import axios from "axios";
+import TagSelect from "../../components/TagSelect/TagSelect";
+import FoodTable from "./FoodTable";
+import { APP_URL } from "../../constants";
+import Modal from "../../components/Modal";
 
 function FoodForm(props) {
   const fetchFood = async () => {
-    await axios.get(APP_URL + 'foods').then(res => {
+    await axios.get(APP_URL + "foods").then(res => {
       const foodData = res.data;
       setFoodData(foodData.data);
     });
   };
   const fetchCategories = async () => {
-    await axios.get(APP_URL + 'categories').then(res => {
+    await axios.get(APP_URL + "categories").then(res => {
       const categoriesData = res.data;
       setCategories(categoriesData.data);
     });
   };
 
   const initialState = {
-    name: '',
-    description: '',
-    category: '',
+    name: "",
+    description: "",
+    category: "",
     dog_food: false,
     edible: false,
-    tags: [],
+    tags: []
   };
   const [categories, setCategories] = useState([]);
   const [foodData, setFoodData] = useState([]);
@@ -37,10 +36,12 @@ function FoodForm(props) {
   const [dogfood, setDogfood] = useToggle(true);
   const [newOpen, setNewOpen] = useState(false);
   const [tags, setTags] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const [edit, setEdit] = useState(false);
   const [show, setModal] = useState(false);
+
+  const [selectValue, setSelectValue] = useState("");
 
   const changeState = food => {
     setEdit(true);
@@ -57,23 +58,23 @@ function FoodForm(props) {
   const onChange = e => {
     setFood({
       ...food,
-      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value
     });
   };
   const handleEdible = () => {
     setFood({
       ...food,
-      edible: edible,
+      edible: edible
     });
   };
   const handleDogfood = () => {
     setFood({
       ...food,
-      dog_food: dogfood,
+      dog_food: dogfood
     });
   };
   const deleteFood = id => {
-    if (window.confirm('Press a button!')) {
+    if (window.confirm("Press a button!")) {
       return axios.delete(`http://localhost:5000/api/foods/${id}`).then(res => {
         setFoodData(foodData.filter(food => food._id !== id));
       });
@@ -110,21 +111,35 @@ function FoodForm(props) {
   const selectTags = newTags => {
     if (tags) setTags(newTags);
   };
-
-  const filterFood = () => {
-    const filter = foodData.filter(food => {
-      let f =
-        food.name.toLowerCase() +
-        food.description.toLowerCase() +
-        food.tags.join(',').toLowerCase() +
-        food.category.toLowerCase();
-      return f.indexOf(search.toLowerCase()) !== -1;
-    });
-    return filter;
+  const setSelect = select => {
+    setSelectValue(select);
   };
+  const filterFood = select => {
+    if (select) {
+      const selectFilter = select
+        ? foodData.filter(food => {
+            return food.category.toLowerCase() === select.toLowerCase();
+            // return select.indexOf(search.toLowerCase()) !== -1;
+          })
+        : null;
+      return selectFilter;
+    } else {
+      return foodData.filter(food => {
+        let f =
+          food.name.toLowerCase() +
+          food.description.toLowerCase() +
+          food.tags.join(",").toLowerCase() +
+          food.category.toLowerCase();
+        return f.indexOf(search.toLowerCase()) !== -1;
+      });
+    }
+  };
+
   useEffect(() => {
     filterFood();
+    setSelect();
   }, [search, foodData]);
+
   const showModal = e => {
     setModal(!show);
   };
@@ -133,7 +148,7 @@ function FoodForm(props) {
       <h1>{props.title}</h1>
       {!show ? (
         <button
-          style={{ float: 'right' }}
+          style={{ float: "right" }}
           onClick={e => {
             setModal(e);
           }}
@@ -181,7 +196,7 @@ function FoodForm(props) {
             </select>
             <div className="radio">
               <label className="label-contain">
-                Is it edible? {food.edible ? 'yes' : 'no'}
+                Is it edible? {food.edible ? "yes" : "no"}
                 <label className="switch">
                   <input
                     onChange={() => {
@@ -198,7 +213,7 @@ function FoodForm(props) {
             </div>
             <div className="radio">
               <label className="label-contain">
-                Is it in dog food? {food.dog_food ? 'yes' : 'no'}
+                Is it in dog food? {food.dog_food ? "yes" : "no"}
                 <label className="switch">
                   <input
                     onChange={() => {
@@ -224,8 +239,18 @@ function FoodForm(props) {
           setSearch(e.target.value);
         }}
       ></input>
+      <select
+        name="category"
+        onChange={e => setSelect(e.currentTarget.value)}
+        required
+      >
+        <option value={0}>Select Category</option>
+        {categories.map(c => (
+          <option value={c.name}>{c.name}</option>
+        ))}
+      </select>
       <FoodTable
-        foods={filterFood()}
+        foods={filterFood(selectValue)}
         editRow={food => changeState(food)}
         deleteFood={id => deleteFood(id)}
       />
