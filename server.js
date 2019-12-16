ba1e686038f428ca0a773afce88da9a3fe9aa286
David Nyman <davenyman@gmail.com>
2019-12-16 13:26:05 -0500

    add heroku

---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const db = require('./server/db');
+
+const routes = require('./server/routes');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+// increase the limit
+myEmitter.setMaxListeners(11);
+
+myEmitter.emit('event');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/test', (req, res) => {
+  res.json({ test: 'test' });
+});
+// app.use('/api', foodRouter);
+app.use('/api/foods', routes.foodRouter);
+app.use('/api/tags', routes.tagRouter);
+app.use('/api/categories', routes.categoryRouter);
+app.use('/api/queries', routes.queryRouter);
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+// Handle React routing by returning all requests to client
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
