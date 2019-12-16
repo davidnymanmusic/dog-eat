da44f523ff44adfcb7d9b6fe6a1bb46b2d29d37d
David Nyman <davenyman@gmail.com>
2019-12-16 13:36:02 -0500

    fix url, add postbuild script

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const APP_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000/api/'
-    : 'https://dogeat.netlify.com/api/';
+    : 'https://dog-eat.herokuapp.com/api/';
