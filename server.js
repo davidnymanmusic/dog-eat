90a88e1b0a3f8cd53b339be336436f12c7c09a24
David Nyman <davenyman@gmail.com>
2019-12-16 14:33:56 -0500

    Fix proxy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ myEmitter.setMaxListeners(11);
 myEmitter.emit('event');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8888;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, '..', 'build')));
 
 // Handle React routing by returning all requests to client
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const APP_URL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000/api/'
+    ? 'http://localhost:8888/api/'
     : 'https://dog-eat.herokuapp.com/api/';
